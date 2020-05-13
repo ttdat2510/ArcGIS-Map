1c89367ec99bd3b2aff576c29be0056b899c359a
Tran Dat <ttdat2510@gmail.com>
2020-05-13 09:45:24 +0700

    Update basic

--- app/src/main/java/com/example/arcgis_map/MainActivity.java
+++ app/src/main/java/com/example/arcgis_map/MainActivity.java
@@ -4,11 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
 
+public class MainActivity extends AppCompatActivity {
+  private MapView mMapView;
+
+  private void setupMap() {
+    if (mMapView != null) {
+      Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+      double latitude = 34.0270;
+      double longitude = -118.8050;
+      int levelOfDetail = 13;
+      ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+      mMapView.setMap(map);
+    }
+  }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    // *** ADD ***
+    mMapView = findViewById(R.id.mapView);
+    setupMap();
+  }
+  @Override
+  protected void onPause() {
+    if (mMapView != null) {
+      mMapView.pause();
+    }
+    super.onPause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    if (mMapView != null) {
+      mMapView.resume();
+    }
+  }
+
+  @Override
+  protected void onDestroy() {
+    if (mMapView != null) {
+      mMapView.dispose();
+    }
+    super.onDestroy();
   }
 }
